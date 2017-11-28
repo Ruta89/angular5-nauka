316ddbe9f0bf6a375ff1778c5977e6999292fe25
Arek Ruta <arek.ruta89@gmail.com>
2017-11-28 10:33:17 +0100

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   tasksList: Array<string> = [];
   tasksDone: Array<string> = [];
+
+  ngOnInit() {
+    this.tasksList = ['zad1', 'zad2', 'zad3', 'zad4'];
+  }
 
   add(task: string) {
     this.tasksList.push(task);
