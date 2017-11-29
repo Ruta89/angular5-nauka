33dfecea4a9960d9e8f9c2eb279bd2ef3a27ad87
Arek Ruta <arek.ruta89@gmail.com>
2017-11-29 23:55:41 +0100

    ElementRef Renderer2

--- src/app/shared/checked.directive.ts
+++ src/app/shared/checked.directive.ts
@@ -1,8 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, Renderer2, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appChecked]'
 })
-export class CheckedDirective {
-  constructor() {}
+export class CheckedDirective implements OnInit {
+  constructor(private el: ElementRef, private render: Renderer2) {}
+
+  ngOnInit() {
+    let li = this.el.nativeElement;
+    this.render.setStyle(
+      li,
+      'list-style-image',
+      'url(/assets/img/checked.png)'
+    );
+    this.render.setStyle(li, 'background', 'aquamarine');
+  }
 }
