52e4356eab97835e45690be3631f04e47b30dfc8
Arkadiusz Ruta <arek.ruta@gmail.com>
2018-01-04 11:39:19 +0100

    dodawanie argumentu (wykrzykinik todo-tassk i string w todo-done)

--- src/app/shared/transform-task.pipe.ts
+++ src/app/shared/transform-task.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'transformTask'
 })
 export class TransformTaskPipe implements PipeTransform {
-  transform(value: string, args?: any): any {
-    return value.charAt(0).toUpperCase() + value.slice(1);
+  transform(value: string, args: string = ''): any {
+    return value.charAt(0).toUpperCase() + value.slice(1) + args;
   }
 }
