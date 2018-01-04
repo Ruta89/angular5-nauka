48b2a9cba4016720a3b2139e30d217c2fe1cd257
Arkadiusz Ruta <arek.ruta@gmail.com>
2018-01-04 11:28:44 +0100

    wyswietlanie pierwszej litery z duzej litery

--- src/app/shared/transform-task.pipe.ts
+++ src/app/shared/transform-task.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'transformTask'
 })
 export class TransformTaskPipe implements PipeTransform {
-  transform(value: any, args?: any): any {
-    return value;
+  transform(value: string, args?: any): any {
+    return value.charAt(0).toUpperCase() + value.slice(1);
   }
 }
